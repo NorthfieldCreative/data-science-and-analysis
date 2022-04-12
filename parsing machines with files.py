81043bb953d319307e7bab04eec8deffc200e64f
northfieldcreative <andrewjmiller56@gmail.com>
2022-04-12 13:30:22 -0600

    update

---
+++ parsing machines with files.py
@@ -0,0 +1,19 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+
+########################################################
+# Open the exported list of assets with cves from tenable
+#########################################################
+machines = pd.read_csv('warfiles.csv', low_memory=False)
+
+###########
+# Because hosts with more than one cve are comma separated within the same 
+# row, we need to Separate all cves respective rows maintaining other attributes
+###########
+
+machines["values(ComputerName)"]=machines["values(ComputerName)"].str.split(' ')
+machinesseparated=(machines.explode("values(ComputerName)").reset_index(drop=True))
+machinesseparated.to_csv(r'warfilesfound.csv', index = False)
